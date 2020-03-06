bba57f39bc4b46e47a130d8441aa47ad93682e56
96StanleyLee <96stanleylee@gmail.com>
2020-03-05 20:54:40 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application
+
+
+
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+    
+        if req.path.match(/items/)
+          item_search = req.path.split("/items/").last
+            
+         if item_found = @@items.find{|item| item.name == item_search}
+            resp.write (item_found.price)
+          else 
+            resp.status = 400
+            resp.write "Item not found"
+          end
+        else
+          resp.status=404
+          resp.write "Route not found"
+        end
+        resp.finish
+      end
+    
+end
